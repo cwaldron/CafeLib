e847c18cd3cd447b3250d2e2d1c9467272443e24
cwaldron <chris.waldron@eroad.com>
2019-07-12 15:28:22 +1200

    Added support for master-detail page.

---
+++ Mobile/CafeLib.Mobile/Views/BaseMasterDetailPage.cs
@@ -0,0 +1,37 @@
+﻿using CafeLib.Mobile.ViewModels;
+using Xamarin.Forms;
+
+// ReSharper disable UnusedMember.Global
+
+namespace CafeLib.Mobile.Views
+{
+    public abstract class BaseMasterDetailPage : MasterDetailPage, IPageBase
+    {
+        public TViewModel GetViewModel<TViewModel>() where TViewModel : BaseViewModel
+        {
+            return (TViewModel)BindingContext;
+        }
+
+        public void SetViewModel<TViewModel>(TViewModel viewModel) where TViewModel : BaseViewModel
+        {
+            BindingContext = viewModel;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            GetViewModel<BaseViewModel>().AppearingCommand.Execute(null);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            GetViewModel<BaseViewModel>().DisappearingCommand.Execute(null);
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            return GetViewModel<BaseViewModel>().BackButtonPressed.Execute(null);
+        }
+    }
+}
