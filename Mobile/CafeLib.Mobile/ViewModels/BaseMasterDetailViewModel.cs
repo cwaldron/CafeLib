642f89344de4a4bc25f44e9e8b2de24ff35adfe9
cwaldron <chris.waldron@eroad.com>
2019-07-15 11:17:51 +1200

    Added BaseMasterDetailViewModel

---
+++ Mobile/CafeLib.Mobile/ViewModels/BaseMasterDetailViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+// ReSharper disable UnusedMember.Global
+
+namespace CafeLib.Mobile.ViewModels
+{
+    public abstract class BaseMasterDetailViewModel<TObject> : BaseViewModel<TObject> where TObject : class
+    {
+        protected BaseMasterDetailViewModel()
+        {
+            PresentedCommand = new Command(() => { });
+        }
+
+        private bool _isPresented;
+        public bool IsPresented
+        {
+            get => _isPresented;
+            set
+            {
+                if (SetValue(ref _isPresented, value))
+                    _presentedCommand.Execute(_isPresented);
+            }
+        }
+
+        /// <summary>
+        /// Appearing command.
+        /// </summary>
+        private ICommand _presentedCommand;
+        public ICommand PresentedCommand
+        {
+            get => _presentedCommand;
+            set => SetValue(ref _presentedCommand, value);
+        }
+    }
+
+    public abstract class BaseMasterDetailViewModel : BaseMasterDetailViewModel<object>
+    {
+    }
+}
