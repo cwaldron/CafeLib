88ed49833c0774ccecac4c8c45d4cc5d90a3477e
cwaldron <chris.waldron@eroad.com>
2019-08-09 11:12:31 +1200

    Removed NavigateAsync extension methods.

--- Mobile/CafeLib.Mobile/Extensions/NavigationExtensions.cs
+++ Mobile/CafeLib.Mobile/Extensions/NavigationExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq;
-using System.Threading.Tasks;
 using CafeLib.Mobile.Services;
 using CafeLib.Mobile.ViewModels;
 using CafeLib.Mobile.Views;
@@ -112,34 +111,9 @@ namespace CafeLib.Mobile.Extensions
         {
             Application.Current.Resolve<IDeviceService>().RunOnMainThread(async () =>
             {
-                await navigation.NavigateAsync(viewModel, animate);
+                await viewModel.InitAsync();
+                await navigation.PushAsync(viewModel, animate);
             });
-        }
-
-        /// <summary>
-        /// Navigate to view model.
-        /// </summary>
-        /// <typeparam name="T">view model type</typeparam>
-        /// <param name="navigation">navigation service</param>
-        /// <param name="animate">transition animation flag</param>
-        /// <returns></returns>
-        public static async Task NavigateAsync<T>(this INavigationService navigation, bool animate = false) where T : BaseViewModel
-        {
-            await navigation.NavigateAsync(Application.Current.Resolve<IPageService>().ResolveViewModel<T>(), animate);
-        }
-
-        /// <summary>
-        /// Asynchronously adds page to the top of the navigation stack.
-        /// </summary>
-        /// <typeparam name="T">view model type</typeparam>
-        /// <param name="navigation">navigation service</param>
-        /// <param name="viewModel">view model</param>
-        /// <param name="animate">optional animation</param>
-        /// <returns></returns>
-        public static async Task NavigateAsync<T>(this INavigationService navigation, T viewModel, bool animate = false) where T : BaseViewModel
-        {
-            await viewModel.InitAsync();
-            await navigation.PushAsync(viewModel, animate);
         }
 
         /// <summary>
@@ -169,38 +143,9 @@ namespace CafeLib.Mobile.Extensions
         {
             Application.Current.Resolve<IDeviceService>().RunOnMainThread(async () =>
             {
-                await navigation.NavigateAsync(viewModel, parameter, animate);
+                await viewModel.InitAsync(parameter);
+                await navigation.PushAsync(viewModel, animate);
             });
-        }
-
-        /// <summary>
-        /// Navigate to view model.
-        /// </summary>
-        /// <typeparam name="T">view model type</typeparam>
-        /// <typeparam name="TP">view model parameter type</typeparam>
-        /// <param name="navigation">navigation service</param>
-        /// <param name="parameter">view model parameter</param>
-        /// <param name="animate">transition animation flag</param>
-        /// <returns></returns>
-        public static async Task NavigateAsync<T, TP>(this INavigationService navigation, TP parameter, bool animate = false) where T : BaseViewModel<TP> where TP : class
-        {
-            await navigation.NavigateAsync(Application.Current.Resolve<IPageService>().ResolveViewModel<T>(), parameter, animate);
-        }
-
-        /// <summary>
-        /// Navigate to view model.
-        /// </summary>
-        /// <typeparam name="T">view model type</typeparam>
-        /// <typeparam name="TP">view model parameter type</typeparam>
-        /// <param name="navigation">navigation service</param>
-        /// <param name="viewModel">view model</param>
-        /// <param name="parameter">view model parameter</param>
-        /// <param name="animate">transition animation flag</param>
-        /// <returns></returns>
-        public static async Task NavigateAsync<T, TP>(this INavigationService navigation, T viewModel, TP parameter, bool animate = false) where T : BaseViewModel<TP> where TP : class
-        {
-            await viewModel.InitAsync(parameter);
-            await navigation.PushAsync(viewModel, animate);
         }
 
         /// <summary>
